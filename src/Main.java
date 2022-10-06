2ada70c1e9e3d486cdbfcbfd3b526d79b04ef793
Nicolai Christmann <nicolai.christmann@gmail.com>
2022-10-06 12:48:36 +0200

    Added method changeIntToChar inorder to append the int as a char to the character object arrayList Added method changeToDecimal, seperates the last program into its own ,method, since to continue changing the base you need to first change to decimal Added division in main to change the number to the desired base.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -15,24 +17,50 @@ public class Main {
             throw new Exception("Unexpected special character");
         } //End if
     } //End Method changeCharToInt
+    public static char changeIntToChar(int x){
+        if (x < 10){
+            return Character.forDigit(x, 10);
+        }else{
+            return (char) (x + 55);
+        }//End if
+    } //End Method changeIntToChar
+    public static int changeToDecimal(String numberString, int base) throws Exception {
+        int newNum = 0;
+        for (int i = 0; i < numberString.length(); i++) {
+            newNum = newNum * base;
+            newNum = newNum + changeCharToInt(numberString.charAt(i));
+        } //End of for
+        System.out.println("The number in the decimal system is: " + newNum);
+        return newNum;
+    }//End Method changeToDecimal
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number you want to convert: "); //Needs to be a string so I can add numbers over base 10 i.e. F for 15
+        System.out.print("Enter the number you want to convert: "); //Needs to be a string, so I can add numbers over base 10 i.e. F for 15
         String numberstrToConvert = sc.nextLine();
         System.out.print("Enter the numbers current base: ");
         int currentBase = sc.nextInt();
         //Should check that no number is used that is greater than the base - user error
-        /*
-        //System.out.print("Enter the base you want the number to be: ");
-        //int baseConv = sc.nextInt();
-            Not sure it works the other way around
-         */
-        for (int i = 0, newNum = 0; i < numberstrToConvert.length(); i++) {
-            newNum = newNum * currentBase;
-            newNum = newNum + changeCharToInt(numberstrToConvert.charAt(i));
-            System.out.println(newNum);
-        } //End of for THIS IS THE HORNER SCHEMA
-        //return newNum; //this goes nowhere right now
+        System.out.print("Enter the base you want the number to be: ");
+        int baseConv = sc.nextInt();
+        int quotient = changeToDecimal(numberstrToConvert, currentBase);
+        int rest = 0;
+        ArrayList<Character> convertedNum = new ArrayList<>();
+        while(!(quotient == 0)){
+            if (quotient < 0){
+                throw new Exception("Error");
+            } else{
+                rest = quotient % baseConv;
+                convertedNum.add(0,changeIntToChar(rest));
+                quotient = quotient / baseConv;
+            }//end if
+            //change to decimal
+            //divide to closest whole number
+            //add the rest to a list or str
+            //take current division solution and repeat
+        }//End while
+        System.out.print("Your number: " + numberstrToConvert + " from base: " + currentBase + " is: ");
+        Arrays.stream(convertedNum.toArray()).forEach(System.out::print);
+        System.out.print(" in the base: " + baseConv + "\n");
     } //End Method Main
 } //End Class Main
 
